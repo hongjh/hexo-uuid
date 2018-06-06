42627e1b3450655d114acb8fb4aafd69dbb866a6
Hong JianHong <hongjianhong@gmail.com>
2018-06-06 17:47:54 +0800

    Update index.js
    add filter new_post_path

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const HexoUuid = require('./lib/hexo-uuid');
 
 hexo.on('new', HexoUuid.newPost);
 
+// new_post_path 要提升优先级5，默认是10
+hexo.extend.filter.register('new_post_path', HexoUuid.new_post_path, 5);
 hexo.extend.filter.register('before_post_render', HexoUuid.before_renderPost);
